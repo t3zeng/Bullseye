063dcede5e0cafe19f512e1b121da5c31b400500
Tian Zeng <t3zeng@gmail.com>
2021-03-17 21:15:01 -0700

    Initial Commit

---
+++ Bullseye/BullseyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BullseyeApp.swift
+//  Bullseye
+//
+//  Created by Tianyu Zeng on 3/17/21.
+//
+
+import SwiftUI
+
+@main
+struct BullseyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
